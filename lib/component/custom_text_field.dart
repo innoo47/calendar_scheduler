4aaecb6ea056c8c950fc45d734c8974ed56e57d6
Inu <i0n4h0o7@gmail.com>
2024-10-18 14:16:21 +0900

    작성 바텀 시트 제작

---
+++ lib/component/custom_text_field.dart
@@ -0,0 +1,44 @@
+import 'package:calendar_scheduler/const/colors.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class CustomTextField extends StatelessWidget {
+  final String label;
+  final bool isTime;
+
+  const CustomTextField({super.key, required this.label, this.isTime = false});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          label,
+          style: TextStyle(
+            fontWeight: FontWeight.w600,
+            color: PRIMARY_COLOR,
+          ),
+        ),
+        Expanded(
+          flex: isTime ? 0 : 1,
+          child: TextFormField(
+            cursorColor: Colors.grey,
+            maxLines: isTime ? 1 : null,
+            expands: !isTime,
+            keyboardType:
+                isTime ? TextInputType.number : TextInputType.multiline,
+            inputFormatters:
+                isTime ? [FilteringTextInputFormatter.digitsOnly] : [],
+            decoration: InputDecoration(
+              border: InputBorder.none,
+              filled: true,
+              fillColor: Colors.grey[300],
+              suffixText: isTime ? '시' : null,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
